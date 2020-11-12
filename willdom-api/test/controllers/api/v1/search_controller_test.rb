66ae8c14f7abe837d5f1699f26dd6c453b46988c
hectorthiele <hectorthiele@hotmail.com>
2020-11-12 20:41:44 -0300

    adding specs

--- willdom-api/test/controllers/api/v1/search_controller_test.rb
+++ willdom-api/test/controllers/api/v1/search_controller_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class Api::V1::SearchControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include SearchMock
+
+  test "should return empty array for no params provided" do
+    get api_v1_search_url, as: :as_json
+    assert_response :success
+    results = JSON.parse(self.response.body)
+
+    puts results
+
+    assert_empty results, 'results is not empty'
+  end
+
+  test "should return results for google" do
+    SearchEngine.any_instance.stubs(:perform).returns(google_results)
+    get api_v1_search_url, as: :as_json, params: { engine: 'google', text: 'willdom' }
+    assert_response :success
+    results = JSON.parse(self.response.body)
+
+    assert_not_empty results, 'results is empty or not present'
+    assert_equal 'google', results[0]["source"], 'The source is not from google'
+  end
+
+  test "should return results for bing" do
+    SearchEngine.any_instance.stubs(:perform).returns(bing_results)
+    get api_v1_search_url, as: :as_json, params: { engine: 'bing', text: 'willdom' }
+    assert_response :success
+    results = JSON.parse(self.response.body)
+
+    assert_not_empty results, 'results is empty or not present'
+    assert_equal 'bing', results[0]["source"], 'The source is not from bing'
+  end
+
+  test "should return results for google and bing" do
+    all_results = google_results + bing_results
+    SearchEngine.any_instance.stubs(:perform).returns(all_results)
+    get api_v1_search_url, as: :as_json, params: { engine: 'google_bing', text: ' Ruby on Rails' }
+    assert_response :success
+    results = JSON.parse(self.response.body)
+
+    assert_not_empty results, 'results is empty or not present'
+    sources = results.map {|result| result['source']}
+
+    assert_includes sources, "bing", 'Bing was not included'
+    assert_includes sources, "google", 'Google was not included'
+
+  end
+
 end
--- willdom-api/test/test_helper.rb
+++ willdom-api/test/test_helper.rb
@@ -1,7 +1,30 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'mocha/minitest'
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
+end
+
+module SearchMock
+  def google_results
+    [
+      {
+        source: "google",
+        title: "Text",
+        link: "#"
+      }
+    ]
+  end
+
+  def bing_results
+    [
+        {
+            source: "bing",
+            title: "Text",
+            link: "#"
+        }
+    ]
+  end
 end
